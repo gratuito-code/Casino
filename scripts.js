08ee6d4adafc8233c8cfa773cc5f00c38c57f3f3
gratuito-code <gratuitotech@gmail.com>
2021-03-31 23:19:49 +0200

    added Hold points funtionalities

--- scripts.js
+++ scripts.js
@@ -3,40 +3,35 @@ let die = document.querySelector(".die");
 let RollBtn = document.getElementById("roll");
 let keepBtn = document.getElementById("keep");
 let resetBtn = document.getElementById("reset");
-let player1CurrentScore = 0;
-let player2CurrentScore = 0;
-let player1TotalScore = 0;
-let player2TotalScore = 0;
-let activePlayer = [0, 1];
-let active = true;
-let playerEl;
-let playerPointsEl;
-let currentEl;
-let overlay;
-let switchPlayers = (player, score) => {
-  playerEl = document.querySelector(`.player--${player}`);
-  playerPointsEl = document.querySelector(`.p-points-${player}`);
-  currentEl = document.querySelector(`.c-points-${player}`);
-  overlay = document.querySelector(`.overlay--${player}`);
-  overlay.classList.toggle("hidden");
-  playerPointsEl.textContent = score;
-};
-//Roll A dice button
+let playerCurrentScore = 0;
+let playerTotalScore = 0;
+let scores = [0, 0];
+let active = 0;
+
+let switchPlayer = () => {
+  playerCurrentScore = 0;
+  active = active === 0 ? 1 : 0;
+  document.querySelector(".overlay--0").classList.toggle("hidden");
+  document.querySelector(".overlay--1").classList.toggle("hidden");
+}; //Roll A dice button
 RollBtn.addEventListener("click", () => {
   let secNumber = Math.trunc(Math.random() * 6) + 1;
-  if (secNumber !== 1) {
-    player1CurrentScore += secNumber;
-  } else {
-    player1CurrentScore = 0;
-    player2CurrentScore += secNumber;
-  }
-  current1El.textContent = player1CurrentScore;
-  current2El.textContent = player2CurrentScore;
   document.getElementById("diceImg").src = `images/dice--${secNumber}.png`;
   die.classList.remove("hidden");
+  if (secNumber !== 1) {
+    playerCurrentScore += secNumber;
+    document.getElementById(
+      `current-pts--${active}`
+    ).textContent = playerCurrentScore;
+  } else {
+    document.getElementById(`current-pts--${active}`).textContent = 0;
+    playerCurrentScore = 0;
+    switchPlayer();
+  }
 });
 
 keepBtn.addEventListener("click", () => {
-  player1TotalScore += player1CurrentScore;
-  player1PointsEl.textContent = player1TotalScore;
+  scores[active] += playerCurrentScore;
+  document.querySelector(`.p-points-${active}`).textContent = scores[active];
+  switchPlayer();
 });
