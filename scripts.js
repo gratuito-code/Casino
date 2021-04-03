4754442d242b184f15f293487154b847da0a81d0
gratuito-code <gratuitotech@gmail.com>
2021-04-03 15:06:21 +0200

    full funtionality

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@ let die = document.querySelector(".die");
 let RollBtn = document.getElementById("roll");
 let keepBtn = document.getElementById("keep");
 let resetBtn = document.getElementById("reset");
+let rollWrapper = document.querySelector(".roll-btn");
+let keepWrapper = document.querySelector(".keep-btn");
 let playerCurrentScore = 0;
-let playerTotalScore = 0;
 let scores = [0, 0];
 let active = 0;
 
@@ -33,5 +34,27 @@ RollBtn.addEventListener("click", () => {
 keepBtn.addEventListener("click", () => {
   scores[active] += playerCurrentScore;
   document.querySelector(`.p-points-${active}`).textContent = scores[active];
+  if (scores[active] >= 10) {
+    document.querySelector(`.p-header-${active}`).textContent = "Winner!!";
+    rollWrapper.classList.add("hidden");
+    keepWrapper.classList.add("hidden");
+    die.classList.add("hidden");
+  } else {
+    document.getElementById(`current-pts--${active}`).textContent = 0;
+    playerCurrentScore = 0;
+    switchPlayer();
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  scores = [0, 0];
+  rollWrapper.classList.remove("hidden");
+  keepWrapper.classList.remove("hidden");
+  document.querySelector(`.p-header-0`).textContent = "Munezero";
+  document.querySelector(`.p-header-1`).textContent = "Dodos";
+  document.getElementById(`current-pts--0`).textContent = 0;
+  document.getElementById(`current-pts--1`).textContent = 0;
+  document.querySelector(`.p-points-0`).textContent = "0";
+  document.querySelector(`.p-points-1`).textContent = "0";
   switchPlayer();
 });
